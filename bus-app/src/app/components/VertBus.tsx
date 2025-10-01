d83e686804c9177db6ab2121f63f522106d0723f
amongsusman <149024306+amongsusman@users.noreply.github.com>
2025-09-30 23:39:22 -0400

    Create VertBus.tsx

---
+++ bus-app/src/app/components/VertBus.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { propagateServerField } from 'next/dist/server/lib/render-server';
+import { useState } from 'react';
+import { ReactNode } from 'react';
+
+type MyBoxProps = {
+  children: ReactNode;
+};
+
+export default function VertBus({ children }: MyBoxProps) {
+    return (
+        <div className=""
+            style={{
+            width: '100%',
+            height: '100%',
+            color: 'white',
+            display: 'flex',
+            alignItems: 'center',
+            background: 'black',
+            justifyContent: 'center',
+            fontWeight: 'bold',
+            fontSize: '1.5rem',
+            boxShadow: '0 4px 12px rgba(0,0,0,0.1)',
+            border: '1px solid red' 
+            }}>
+        {children}
+        </div>
+    );
+}
