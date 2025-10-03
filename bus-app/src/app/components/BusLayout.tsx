5225aab080939f336abac30b5bb74a6d3992b085
amongsusman <149024306+amongsusman@users.noreply.github.com>
2025-10-03 00:19:10 -0400

    Update BusLayout.tsx

--- bus-app/src/app/components/BusLayout.tsx
+++ bus-app/src/app/components/BusLayout.tsx
@@ -16,7 +16,7 @@ export default function BusLayout() {
                 boxShadow: '0 4px 12px rgba(0,0,0,0.1)',
                 border: '1px solid red' 
             }}>
-            <div className="justify-center flex gap-2 w-full">
+            <div className="justify-center flex gap-2 w-full" style={{border: '1px solid red'}}>
                 <div className=""
                     style={{
                     width: '50%',
@@ -37,8 +37,8 @@ export default function BusLayout() {
                     <VertBus>2</VertBus>
                 </div>
             </div>
-            <div className = "justify-center flex gap-2 w-full h-[7.5%]">
-                <div className=""
+            <div className = "justify-center flex gap-2 w-full h-[7.5%]" style={{border: '1px solid red'}}>
+                <div className="ml-12 sm:ml-16 md:ml-10 xl:ml-17"
                     style={{
                     width: '40%',
                     color: 'white',
@@ -52,6 +52,26 @@ export default function BusLayout() {
                     border: '1px solid red' 
                 }}>
                     Building 2
+                </div>
+                <div className = "flex flex-col w-1/20 gap-2">
+                    <VertBus>3</VertBus>
+                </div>
+            </div>
+            <div className = "justify-center flex gap-2 w-full h-[35%]" style={{border: '1px solid red'}}>
+                <div className=""
+                    style={{
+                    width: '10%',
+                    color: 'white',
+                    display: 'flex',
+                    alignItems: 'center',
+                    background: 'black',
+                    justifyContent: 'center',
+                    fontWeight: 'bold',
+                    fontSize: '1.5rem',
+                    boxShadow: '0 4px 12px rgba(0,0,0,0.1)',
+                    border: '1px solid red' 
+                }}>
+                    <span className="rotate-270" style={{border:'1px solid red'}}>Building 3</span>
                 </div>
                 <div className = "flex flex-col w-1/20 gap-2">
                     <VertBus>3</VertBus>
